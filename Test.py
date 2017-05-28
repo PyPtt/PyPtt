c1fd23f262e1773b03cd4f73fc263532b55e01e9
Truth0906 <thetruth0906@livemail.tw>
2017-05-29 01:06:52 +0800

    Fixed post many times in one login error

--- Test.py
+++ Test.py
@@ -3,12 +3,24 @@ import PTTTelnetCrawlerLibrary
 
 def Post(ID, PW, KickOtherLogin, Board, Title, Content):
     
-    ptt = PTTTelnetCrawlerLibrary.Ptt(ID, PW, KickOtherLogin)
-    if ptt.isConnected():
-        if ptt.login():
-            ptt.post(Board, Title, Content)
-    ptt.logout()
+    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
+    if PTTCrawler.isConnected():
+        if PTTCrawler.login():
+            PTTCrawler.post(Board, Title + " 1", Content)
+            PTTCrawler.post(Board, Title + " 2", Content)
+            PTTCrawler.post(Board, Title + " 3", Content)
+            PTTCrawler.post(Board, Title + " 4", Content)
+            PTTCrawler.post(Board, Title + " 5", Content)
+    PTTCrawler.logout()
 
+def ListALLPost(ID, PW, KickOtherLogin, Board):
+    
+    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
+    if PTTCrawler.isConnected():
+        if PTTCrawler.login():
+            PTTCrawler.listPost(Board)
+    PTTCrawler.logout()
+    
 if __name__ == "__main__":
     print("Welcome to PTT Telnet Crawler Library Demo")
 
