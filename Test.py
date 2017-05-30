2a53a80d56a83b207ccd5fa4bfc3e2c52deb43d0
Truth0906 <thetruth0906@livemail.tw>
2017-05-30 16:04:12 +0800

    Add getPostInformationByIndex, getNewestPostIndex and getNewPostIndex, add smaples

--- Test.py
+++ Test.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import PTTTelnetCrawlerLibrary
 
 def Post(ID, PW, KickOtherLogin, Board, Title, Content, PostType, SignType):
@@ -19,19 +20,69 @@ def GetPostInformation(ID, PW, KickOtherLogin, Board, PostID):
     if not PTTCrawler.isLoginSuccess():
         return False
     
-    result = PTTCrawler.getPostInformationByID(Board, PostID)
-
+    Post = PTTCrawler.getPostInformationByID(Board, PostID)
+    result = not Post == None
     PTTCrawler.logout()
+    return result
+
+def GetPostInformationByIndex(ID, PW, KickOtherLogin, Board, PostIndex):
+    result = False
+    
+    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
+    if not PTTCrawler.isLoginSuccess():
+        return False
+    
+    Post = PTTCrawler.getPostInformationByIndex(Board, PostIndex)
+    result = not Post == None
+    PTTCrawler.logout()
+    return result
+    
+def GetNewestPostIndex(ID, PW, KickOtherLogin, Board):
+
+    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
+    if not PTTCrawler.isLoginSuccess():
+        return False
+    
+    index = PTTCrawler.getNewestPostIndex(Board)
+    if index == -1:
+        return False
+    print("Index: " + str(index))
+    PTTCrawler.logout()
+    return True
+
+def GetNewPostIndex(ID, PW, KickOtherLogin, Board):
+    result = False
+    
+    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
+    if not PTTCrawler.isLoginSuccess():
+        return False
+    
+    LastIndex = 0
+    LastIndexList = [0]
+    for i in range(30):
+        if not len(LastIndexList) == 0:
+            LastIndex = LastIndexList.pop()
+        LastIndexList = PTTCrawler.getNewPostIndex(Board, LastIndex)
+        
+        if not len(LastIndexList) == 0:
+            for NewPostIndex in LastIndexList:
+                print("Detected new post: " + str(NewPostIndex))
+        time.sleep(2)
+    PTTCrawler.logout()
+    result = True
     return result
     
 if __name__ == "__main__":
     print("Welcome to PTT Telnet Crawler Library Demo")
 
-    ID = 'CodingMan'
-    Password = 'love1214'
+    ID = 'Your PTT ID'
+    Password = 'Your PTT Password'
     KickOtherLogin = False
 
     #發文類別           1
     #簽名檔        	0
     #Post(ID, Password, KickOtherLogin, 'test','發文類別測試', '發文類別測試 QQ', 1, 0)
-    GetPostInformation(ID, Password, KickOtherLogin, 'GO', "1PAIyWdT")
+    #GetPostInformation(ID, Password, KickOtherLogin, 'GO', "1PAIyWdT")
+    #GetPostInformationByIndex(ID, Password, KickOtherLogin, 'Wanted', 68935)
+    GetNewestPostIndex(ID, Password, KickOtherLogin, 'Wanted')
+    #GetNewPostIndex(ID, Password, KickOtherLogin, 'Wanted')
