3afb8d1463cc63ade58ce26b0d0d7dd1db03a6eb
CodingMan <pttcodingman@gmail.com>
2017-06-02 15:14:35 +0800

    Fixed login error, fixed gotoUserMenu error, fixed getNewestPostIndex error

--- Test.py
+++ Test.py
@@ -2,186 +2,90 @@ import sys
 import time
 import PTTTelnetCrawlerLibrary
 
-def GoToBoard(ID, PW, KickOtherLogin, Board):
-    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
-    if not PTTCrawler.isLoginSuccess():
-        return False
-    if PTTCrawler.gotoBoard(Board):
-        print("Go to " + Board + " success")
-    else:
-        print("Go to " + Board + " fail")
-    
-    if PTTCrawler.gotoBoard(Board):
-        print("Go to " + Board + " success")
-    else:
-        print("Go to " + Board + " fail")
-    
-    if PTTCrawler.gotoBoard(Board):
-        print("Go to " + Board + " success")
-    else:
-        print("Go to " + Board + " fail")
-    PTTCrawler.logout()
-
-def Post(ID, PW, KickOtherLogin, Board, Title, Content, PostType, SignType):
-    
-    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
-    if not PTTCrawler.isLoginSuccess():
-        return False
-    PTTCrawler.post(Board, Title + " 1", Content, PostType, SignType)
-    PTTCrawler.post(Board, Title + " 2", Content, PostType, SignType)
-    PTTCrawler.post(Board, Title + " 3", Content, PostType, SignType)
-    PTTCrawler.logout()
-    return True
-def GetPostInformationByID(ID, PW, KickOtherLogin, Board, PostID):
-
-    result = False
-    
-    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
-    if not PTTCrawler.isLoginSuccess():
-        return False
-    
-    Post = PTTCrawler.getPostInformationByID(Board, PostID)
-    result = not Post == None
-    PTTCrawler.logout()
-    return result
-
-def GetPostInformationByIndex(ID, PW, KickOtherLogin, Board, PostIndex):
-    result = False
-    
-    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
-    if not PTTCrawler.isLoginSuccess():
-        return False
-    
-    Post = PTTCrawler.getPostInformationByIndex(Board, PostIndex)
-    result = not Post == None
-    PTTCrawler.logout()
-    return result
-    
-def GetNewestPostIndex(ID, PW, KickOtherLogin, Board):
-
-    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
-    if not PTTCrawler.isLoginSuccess():
-        return False
-    
-    index = PTTCrawler.getNewestPostIndex(Board)
-    if index == -1:
-        print("Fail")
-        return False
-    print("Index: " + str(index))
-    
-    index = PTTCrawler.getNewestPostIndex(Board)
-    if index == -1:
-        print("Fail")
-        return False
-    print("Index: " + str(index))
-    
-    index = PTTCrawler.getNewestPostIndex(Board)
-    if index == -1:
-        print("Fail")
-        return False
-    print("Index: " + str(index))
-    PTTCrawler.logout()
-    return True
-
-def GetNewPostIndex(ID, PW, KickOtherLogin, Board):
-    result = False
-    
-    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
-    if not PTTCrawler.isLoginSuccess():
-        return False
-    
-    LastIndex = 0
-    LastIndexList = [0]
-    for i in range(30):    
-        try:
-            if not len(LastIndexList) == 0:
-                LastIndex = LastIndexList.pop()
-            
-            LastIndexList = PTTCrawler.getNewPostIndex(Board, LastIndex)
-            
-            if not len(LastIndexList) == 0:
-                for NewPostIndex in LastIndexList:
-                    print("Detected new post: " + str(NewPostIndex))
-            time.sleep(2)
-        except KeyboardInterrupt:
-            break;
-    PTTCrawler.logout()
-    result = True
-    return result
-    
-def GetNewestPostInformation(ID, PW, KickOtherLogin, Board):
-    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
-    if not PTTCrawler.isLoginSuccess():
-        return False
-    NewestIndex = PTTCrawler.getNewestPostIndex(Board)
-    
-    Post = PTTCrawler.getPostInformationByIndex(Board, NewestIndex)
-    if Post == None:
-        print("Get information fail")
-        return False
-    print(Post.getPostContent())
-    
-    PTTCrawler.logout()    
-def Push(ID, PW, KickOtherLogin, Board, PushType, PushContent):
-    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
-    if not PTTCrawler.isLoginSuccess():
-        return False
-    
-    NewestPostIndex = 0
-    
-    for i in range(3):
-        NewestPostIndex = PTTCrawler.getNewestPostIndex(Board)
-        if PTTCrawler.pushByIndex(Board, NewestPostIndex, PTTCrawler.PushType_Push, PushContent + " by post index"):
-            print("pushByIndex Push success")
-        else:
-            print("pushByIndex Push fail")
-            
-    for i in range(3):
-        NewPost = PTTCrawler.getPostInformationByIndex(Board, NewestPostIndex)
-        
-        if NewPost == None:
-            print("getPostInformationByIndex fail")
-            break
-        
-        if PTTCrawler.pushByID(Board, NewPost.getPostID(), PTTCrawler.PushType_Push, PushContent + " by post id"):
-            print("pushByID Push success")
-        else:
-            print("pushByID Push fail")
-    PTTCrawler.logout()
-    return True
-
-def Mail(ID, PW, KickOtherLogin, MailTitle, MailContent, SignType):
-    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, PW, KickOtherLogin)
-    if not PTTCrawler.isLoginSuccess():
-            return False
-    
-    for i in range(3):
-        if PTTCrawler.mail(ID, MailTitle, MailContent, SignType):
-            print("Mail to " + ID + " success")
-        else:
-            print("Mail to " + ID + " fail")
-    
-    PTTCrawler.logout()
 if __name__ == "__main__":
     print("Welcome to PTT Telnet Crawler Library Demo")
 
     ID = 'Your PTT ID'
     Password = 'Your PTT Password'
     KickOtherLogin = False
-
-    #發文類別           1
+    PTTCrawler = PTTTelnetCrawlerLibrary.PTTTelnetCrawlerLibrary(ID, Password, KickOtherLogin)
+    if not PTTCrawler.isLoginSuccess():
+        PTTCrawler.Log("Login fail")
+        sys.exit()
+    
+    BoardList = ["Wanted", "AllTogether", "Gossiping"]
+    
+    for Board in BoardList:
+        NewestIndex = PTTCrawler.getNewestPostIndex(Board)
+        if not NewestIndex == -1:
+            PTTCrawler.Log("Get " + Board + " get newest post index success: " + str(NewestIndex))
+        else:
+            PTTCrawler.Log("Get " + Board + " get newest post index fail")
+    
+    for Board in BoardList:
+        if PTTCrawler.gotoBoard(Board):
+            PTTCrawler.Log("Go to " + Board + " success")
+        else:
+            PTTCrawler.Log("Go to " + Board + " fail")
+    
+    #發文類別       1
     #簽名檔        	0
-    #GoToBoard(ID, Password, KickOtherLogin, 'Test')
-    #Post(ID, Password, KickOtherLogin, 'Test','連續自動PO文測試', '自動PO文測試 啾咪', 1, 0)
-    #GetPostInformationByID(ID, Password, KickOtherLogin, 'GO', "1PAIyWdT")
-    #GetPostInformationByIndex(ID, Password, KickOtherLogin, 'Wanted', 68935)
-    #GetNewestPostIndex(ID, Password, KickOtherLogin, 'Wanted')
-    #GetNewPostIndex(ID, Password, KickOtherLogin, 'Wanted')
-    #GetNewestPostInformation(ID, Password, KickOtherLogin, "Wanted")
-    """
-    Push =         1
-    Boo =          2
-    Arrow =        3
-    """
-    #Push(ID, Password, KickOtherLogin, "Test", 1, "偵測最新文章推文測試")
-    Mail(ID, Password, KickOtherLogin, "測試標題", "自動測試 如有誤寄打擾 抱歉QQ", 0)
+
+    for i in range(3):
+        if PTTCrawler.post("Test", "連續自動PO文測試 " + str(i), "自動PO文測試", 1, 0):
+            PTTCrawler.Log("Post success")
+        else:
+            PTTCrawler.Log("Post fail")
+
+    PostIDList = ["1PC1YXYj", "1PCBfel1", "1D89C0oV"]
+    
+    for PostID in PostIDList:
+        Post = PTTCrawler.getPostInformationByID("Wanted", PostID)
+        if not Post == None:
+            PTTCrawler.Log("getPostInformationByID success")
+        else:
+            PTTCrawler.Log("getPostInformationByID fail")
+    
+    NewestIndex = PTTCrawler.getNewestPostIndex("Wanted")
+    
+    for i in range(3):
+        Post = PTTCrawler.getPostInformationByIndex("Wanted", NewestIndex - i)
+        if not Post == None:
+            PTTCrawler.Log("getPostInformationByIndex success: " + str(NewestIndex - i))
+        else:
+            PTTCrawler.Log("getPostInformationByIndex fail: " + str(NewestIndex - i))
+    
+    LastIndex = NewestIndex - 5
+    for i in range(3):
+        #Return new post list LastIndex ~ newest without LastIndex
+        LastIndexList = PTTCrawler.getNewPostIndex("Wanted", LastIndex)
+        if not len(LastIndexList) == 0:
+            for NewPostIndex in LastIndexList:
+                PTTCrawler.Log("Detected new post: " + str(NewPostIndex))
+            LastIndex = LastIndexList.pop()
+    
+    for i in range(3):
+        NewestPostIndex = PTTCrawler.getNewestPostIndex("Test")
+        if PTTCrawler.pushByIndex("Test", NewestPostIndex, PTTCrawler.PushType_Push, "自動推文測試 by post index"):
+            PTTCrawler.Log("pushByIndex Push success")
+        else:
+            PTTCrawler.Log("pushByIndex Push fail")
+            
+    for i in range(3):
+        NewPost = PTTCrawler.getPostInformationByIndex("Test", NewestPostIndex)
+        
+        if NewPost == None:
+            PTTCrawler.Log("getPostInformationByIndex fail")
+            break
+        if PTTCrawler.pushByID("Test", NewPost.getPostID(), PTTCrawler.PushType_Push, "自動推文測試 by post id"):
+            PTTCrawler.Log("pushByID Push success")
+        else:
+            PTTCrawler.Log("pushByID Push fail")
+    #0 不加簽名檔
+    for i in range(3):
+        if PTTCrawler.mail(ID, "自動寄信測試標題", "自動測試 如有誤寄打擾 抱歉QQ", 0):
+            PTTCrawler.Log("Mail to " + ID + " success")
+        else:
+            PTTCrawler.Log("Mail to " + ID + " fail")
+    
+    PTTCrawler.logout()
