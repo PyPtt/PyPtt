811b08fef96ef9bf9866deae2c5abc158a25d33a
CodingMan <pttcodingman@gmail.com>
2017-07-17 14:46:06 +0800

    修正讀檔錯誤

--- PTTUtil.py
+++ PTTUtil.py
@@ -3,7 +3,7 @@ import time
 from time import gmtime, strftime
 
 def Log(InputMessage):
-    TotalMessage = "[" + strftime("%Y-%m-%d %H:%M:%S") + "] " + InputMessage
+    TotalMessage = "[" + strftime("%Y-%m-%d %H:%M:%S") + "]" + InputMessage
     #text.encode(sys.stdout.encoding)
     #print(TotalMessage.encode('utf-8-sig', 'ignore').decode('utf-8-sig'))
     print(TotalMessage.encode(sys.stdin.encoding, "replace").decode(sys.stdin.encoding))
@@ -16,7 +16,7 @@ def readPostFile(FileName):
     try:
         with open(FileName, encoding = 'utf-8-sig') as File:
             Temp = File.readlines()
-            Temp = [x.strip() for x in content]
+            Temp = [x.strip() for x in Temp]
             result = '\r\n'.join(Temp)
     except FileNotFoundError:
         return None
