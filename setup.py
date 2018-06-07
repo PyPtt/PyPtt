cb3283f3a0757c0fd306b24cec3b2631f23b1c4a
CodingMan <pttcodingman@gmail.com>
2018-06-07 18:32:52 +0800

    新增 getErrorCode API

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from PTTLibrary import Version
+
 """A setuptools based setup module.
 
 See:
@@ -11,12 +13,12 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
+# here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#     long_description = f.read()
+Version = Version.Ver
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 
@@ -40,7 +42,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.6.6',  # Required
+    version=Version,  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
