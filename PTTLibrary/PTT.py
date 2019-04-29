1fffaecbe0ae45a85821baacdcc054b5ddfd7614
CodingMan <pttcodingman@gmail.com>
2019-04-29 11:04:11 +0800

    commit

--- PTTLibrary/PTT.py
+++ PTTLibrary/PTT.py
@@ -1,9 +1,30 @@
 import sys
 import time
+import types
+
+try:
+    from . import Util
+    from . import Config
+except:
+    import Util
+    import Config
+
+Version = Config.Version
 
 
-@synchronized
 class Library(object):
 
     def __init__(self):
+        pass
+    
+    def method1(self):
+        pass
+
+    def method2(self):
+        pass
         
+
+if __name__ == '__main__':
+
+    print('PTT Library v ' + Version)
+    print('Developed by PTT CodingMan')
