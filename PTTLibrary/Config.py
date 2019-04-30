6b737ff63d08f2acfac765de4ab4e7d59dedaa21
CodingMan <thetruth0906@gmail.com>
2019-04-30 18:00:31 +0800

    建立 telnet 連線

--- PTTLibrary/Config.py
+++ PTTLibrary/Config.py
@@ -1,5 +1,13 @@
+try:
+    from . import DataType
+except:
+    import DataType
 
-
-Version = '0.8.0 beta'
+Version = '0.8.0 beta '
 Host = 'ptt.cc'
 Port = '23'
+
+RetryWaitTime = 3
+
+Language = DataType.Language.Chinese
+ConnectionMode = DataType.ConnectionMode.Telnet
--- PTTLibrary/PTT.py
+++ PTTLibrary/PTT.py
@@ -1,36 +1,83 @@
 import sys
 import time
-import types
 
 try:
-    from . import Util
     from . import DataType
     from . import Config
+    from . import Util
     from . import i18n
-except:
-    import Util
+    from . import Exceptions
+    from . import Core
+    from . import ErrorCode
+    from . import Synchronize
+except FileNotFoundError:
     import DataType
     import Config
+    import Util
     import i18n
+    import Exceptions
+    import Core
+    import ErrorCode
+    import Synchronize
 
 Version = Config.Version
+ErrorCode = ErrorCode.ErrorCode()
+Language = DataType.Language
+ConnectionMode = DataType.ConnectionMode
 
 
-class Library(Util.SynchronizeAllMethod):
+class Library(Synchronize.SynchronizeAllMethod):
 
-    def __init__(self):
-        pass
+    def __init__(self,
+                 Language: int=0,
+                 ConnectionMode: int=0,
+                 ):
 
-    def login(self):
-        pass
+        if Language == 0:
+            Language = Config.Language
+        elif not Util.checkRange(DataType.Language, Language):
+            raise Exceptions.ParameterError('Unknow language', Language)
+        else:
+            Config.Language = Language
+        i18n.load(Language)
 
+        Util.showValue([
+            i18n.i18n,
+            i18n.Init,
+            ],
+            i18n.Done
+        )
+
+        if ConnectionMode == 0:
+            ConnectionMode = Config.ConnectionMode
+        elif not Util.checkRange(DataType.ConnectionMode, ConnectionMode):
+            Util.showValue(i18n.ErrorParameter, ConnectionMode)
+            return
+        else:
+            Config.ConnectionMode = ConnectionMode
+        self._Core = Core.API(ConnectionMode)
+
+        Util.showValue([
+            i18n.PTT,
+            i18n.Library,
+            ' v ' + Version,
+            i18n.Init,
+            ],
+            i18n.Done
+        )
+
+    def login(self, ID: str, Password: str):
+        ErrCode = self._Core.connect()
+
+        return ErrCode
+        
     def logout(self):
         pass
+
+    def log(self, Msg):
+        Util.log(Msg)
 
 if __name__ == '__main__':
 
     print('PTT Library v ' + Version)
     print('Developed by PTT CodingMan')
-
-    print(i18n.load(DataType.Language.English))
-    print(i18n.ConnectMode)
