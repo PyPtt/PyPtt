fe296d3fef6102cb6ac4a468a8ae6a5d5e2e88bc
CodingMan <thetruth0906@gmail.com>
2019-05-11 22:54:00 +0800

    新增 Screens 管理畫面比對與展示

---
+++ PTTLibrary/Screens.py
@@ -0,0 +1,92 @@
+import sys
+
+try:
+    import DataType
+    import Config
+    import Util
+    import i18n
+    import Log
+except ModuleNotFoundError:
+    from . import DataType
+    from . import Config
+    from . import Util
+    from . import i18n
+    from . import Log
+
+
+class Target(object):
+    MainMenu = [
+        '人, 我是',
+        '[呼叫器]',
+    ]
+
+    QueryPost = [
+        '請按任意鍵繼續',
+        '這一篇文章值',
+    ]
+
+    InBoard = [
+        '文章選讀',
+        '進板畫面',
+        '【板主'
+    ]
+
+    InPost = [
+        '瀏覽',
+        '目前顯示',
+        '離開'
+    ]
+
+    PostEnd = [
+        '瀏覽',
+        '(100%)  目前顯示',
+        '離開'
+    ]
+
+    PostIP = [
+        '※ 發信站: 批踢踢實業坊(ptt.cc), 來自:'
+    ]
+
+    ContentEnd = PostIP
+
+
+def show(ScreenQueue, FunctionName=None):
+    if Config.LogLevel != Log.Level.TRACE:
+        return
+
+    if isinstance(ScreenQueue, list):
+        for Screen in ScreenQueue:
+            print('-' * 50)
+            try:
+                print(Screen.encode(
+                    sys.stdin.encoding, "replace").decode(
+                        sys.stdin.encoding
+                    )
+                )
+            except Exception:
+                print(Screen.encode('utf-8', "replace").decode('utf-8'))
+    else:
+        print('-' * 50)
+        try:
+            print(ScreenQueue.encode(
+                sys.stdin.encoding, "replace").decode(
+                    sys.stdin.encoding))
+        except Exception:
+            print(ScreenQueue.encode('utf-8', "replace").decode('utf-8'))
+
+        print('len:' + str(len(ScreenQueue)))
+    if FunctionName is not None:
+        print('錯誤在 ' + FunctionName + ' 函式發生')
+    print('-' * 50)
+
+
+def isScreenMatch(Screen: str, Target):
+
+    if isinstance(Target, str):
+        return Target in Screen
+    if isinstance(Target, list):
+        for T in Target:
+            if T not in Screen:
+                return False
+        return True
+
