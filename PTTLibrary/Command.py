5f7c61f141c18ea59bbec636c2c34bbea0a6e591
CodingMan <thetruth0906@gmail.com>
2019-05-25 21:27:13 +0800

    新增 PTT 指令集

---
+++ PTTLibrary/Command.py
@@ -0,0 +1,12 @@
+
+Enter = '\r'
+Ctrl_C = '\x03'
+Ctrl_P = '\x10'
+Refresh = '\x0C'
+Ctrl_X = '\x18'
+Up = '\x1b\x4fA'
+Down = '\x1b\x4fB'
+Right = '\x1b\x4fC'
+Left = '\x1b\x4fD'
+QueryPost = 'Q'
+GoMainMenu = Left * 5
