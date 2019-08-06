15cd02fc08442892803370c4dcc33eb201df4ece
CodingMan <thetruth0906@gmail.com>
2019-08-06 23:20:50 +0800

    Add echo server and client sample

--- EchoClient.py
+++ EchoClient.py
@@ -14,33 +14,46 @@ def getPW():
             ID = Account['ID']
             Password = Account['Password']
     except FileNotFoundError:
-        print('Please note PTT ID and Password in Account.txt')
+        print('Please note PTT ID and Password in Account2.txt')
         print('{"ID":"YourID", "Password":"YourPassword"}')
         sys.exit()
 
     return ID, Password
 
 
-def Echo():
+def SendEcho():
 
     OperateType = PTT.WaterBallOperateType.Clear
     WaterBallList = PTTBot.getWaterBall(OperateType)
+
+    PTTBot.setCallStatus(PTT.CallStatus.Off)
+
+    Content = '\n'.join(
+        [str(x % 10) for x in range(10)]
+    )
+    Content = '哈囉!!這是測試水球!!'
+
+    PTTBot.throwWaterBall('CodingMan', Content)
 
     while True:
         WaterBallList = PTTBot.getWaterBall(OperateType)
         if WaterBallList is None:
             time.sleep(1)
             continue
+
+        End = False
         for WaterBall in WaterBallList:
             if not WaterBall.getType() == PTT.WaterBallType.Catch:
                 continue
+            End = True
 
             Target = WaterBall.getTarget()
             Content = WaterBall.getContent()
 
             print(f'收到來自 {Target} 的水球 [{Content}]')
 
-            PTTBot.throwWaterBall(Target, 'Echo: ' + Content)
+        if End:
+            break
 
 
 if __name__ == '__main__':
@@ -54,7 +67,7 @@ if __name__ == '__main__':
         PTTBot = PTT.Library(
             ConnectMode=PTT.ConnectMode.WebSocket,
             # LogLevel=PTT.LogLevel.TRACE,
-            LogLevel=PTT.LogLevel.DEBUG,
+            # LogLevel=PTT.LogLevel.DEBUG,
         )
         try:
             PTTBot.login(
@@ -66,7 +79,7 @@ if __name__ == '__main__':
             PTTBot.log('登入失敗')
             sys.exit()
 
-        Echo()
+        SendEcho()
     except Exception as e:
 
         traceback.print_tb(e.__traceback__)
