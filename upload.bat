c959dac208f6559378474c689edb514a0de3964b
CodingMan <thetruth0906@gmail.com>
2019-08-27 22:30:08 +0800

    Remove pyc files

--- upload.bat
+++ upload.bat
@@ -1,10 +1,13 @@
 @echo off
 cls
 
-rmdir /s /q dist
-rmdir /s /q PTTLibrary.egg-info
+@rmdir /s /q dist
+@rmdir /s /q PTTLibrary.egg-info
 
 python setup.py sdist
+
+@rmdir /s /q PTTLibrary\__pycache__
+
 twine upload dist/*
 
 echo Upload finish
