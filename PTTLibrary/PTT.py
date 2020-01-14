e661a61be8f75fc12dfaac836b9d7ebed09efb5f
CodingMan <CodingMan@uptt.cc>
2020-01-14 17:40:46 +0800

    Add getBoardInfo

--- PTTLibrary/PTT.py
+++ PTTLibrary/PTT.py
@@ -4286,6 +4286,284 @@ class Library:
 
         return resultlist
 
+    def getBoardInfo(
+        self,
+        Board,
+        setting=True
+    ):
+
+        self._OneThread()
+
+        if not self._LoginStatus:
+            raise Exceptions.RequireLogin(i18n.RequireLogin)
+
+        CheckValue.check(str, 'Board', Board)
+        CheckValue.check(bool, 'setting', setting)
+        self._checkBoard(Board)
+
+        CmdList = []
+        CmdList.append(Command.GoMainMenu)
+        CmdList.append('qs')
+        CmdList.append(Board)
+        CmdList.append(Command.Enter)
+        CmdList.append(Command.Ctrl_C * 2)
+        CmdList.append(Command.Space)
+        Cmd = ''.join(CmdList)
+
+        TargetList = [
+            ConnectCore.TargetUnit(
+                i18n.LoginSuccess,
+                [
+                    '文章選讀',
+                    '進板畫面'
+                ],
+                BreakDetect=True
+            ),
+        ]
+
+        index = self._ConnectCore.send(
+            Cmd,
+            TargetList
+        )
+
+        OriScreen = self._ConnectCore.getScreenQueue()[-1]
+        print(OriScreen)
+        Nuser = OriScreen.split('\n')[2]
+        # print(Nuser)
+        pattern = re.compile('[\d]+')
+        OnlineUser = pattern.search(Nuser).group(0)
+        Log.showValue(
+            Log.Level.DEBUG,
+            '人氣',
+            OnlineUser
+        )
+
+        if not setting:
+            return DataType.BoardInfo(Board, OnlineUser)
+
+        TargetList = [
+            ConnectCore.TargetUnit(
+                i18n.AnyKeyContinue,
+                '任意鍵繼續',
+                BreakDetect=True,
+                LogLevel=Log.Level.DEBUG
+            ),
+        ]
+
+        index = self._ConnectCore.send(
+            'i',
+            TargetList
+        )
+
+        OriScreen = self._ConnectCore.getScreenQueue()[-1]
+        # print(OriScreen)
+
+        p = re.compile('中文敘述: (.+)')
+        r = p.search(OriScreen)
+        if r is not None:
+            ChineseDes = r.group(0)[5:].strip()
+        Log.showValue(
+            Log.Level.DEBUG,
+            '中文敘述',
+            ChineseDes
+        )
+
+        p = re.compile('板主名單: (.+)')
+        r = p.search(OriScreen)
+        if r is not None:
+            ModeratorLine = r.group(0)[5:].strip()
+            Moderators = ModeratorLine.split('/')
+        Log.showValue(
+            Log.Level.DEBUG,
+            '板主名單',
+            Moderators
+        )
+
+        OpenState = ('公開狀態(是否隱形): 公開' in OriScreen)
+        Log.showValue(
+            Log.Level.DEBUG,
+            '公開狀態',
+            OpenState
+        )
+
+        IntoTopTenWhenHide = (
+            '隱板時 可以 進入十大排行榜' in OriScreen
+        )
+        Log.showValue(
+            Log.Level.DEBUG,
+            '隱板時可以進入十大排行榜',
+            IntoTopTenWhenHide
+        )
+
+        NonBoardMembersPost = ('開放 非看板會員發文' in OriScreen)
+        Log.showValue(
+            Log.Level.DEBUG,
+            '非看板會員發文',
+            NonBoardMembersPost
+        )
+
+        ReplyPost = ('開放 回應文章' in OriScreen)
+        Log.showValue(
+            Log.Level.DEBUG,
+            '回應文章',
+            ReplyPost
+        )
+
+        SelfDelPost = ('開放 自刪文章' in OriScreen)
+        Log.showValue(
+            Log.Level.DEBUG,
+            '自刪文章',
+            SelfDelPost
+        )
+
+        PushPost = ('開放 推薦文章' in OriScreen)
+        Log.showValue(
+            Log.Level.DEBUG,
+            '推薦文章',
+            PushPost
+        )
+
+        BooPost = ('開放 噓文' in OriScreen)
+        Log.showValue(
+            Log.Level.DEBUG,
+            '噓文',
+            BooPost
+        )
+
+        # 限制 快速連推文章, 最低間隔時間: 5 秒
+        # 開放 快速連推文章
+
+        FastPush = ('開放 快速連推文章' in OriScreen)
+        Log.showValue(
+            Log.Level.DEBUG,
+            '快速連推文章',
+            FastPush
+        )
+
+        if not FastPush:
+            p = re.compile('最低間隔時間: [\d]+')
+            r = p.search(OriScreen)
+            if r is not None:
+                MinInterval = r.group(0)[7:].strip()
+                MinInterval = int(MinInterval)
+            else:
+                MinInterval = 0
+            Log.showValue(
+                Log.Level.DEBUG,
+                '最低間隔時間',
+                MinInterval
+            )
+        else:
+            MinInterval = 0
+
+        # 推文時 自動 記錄來源 IP
+        # 推文時 不會 記錄來源 IP
+        PushRecordIP = ('推文時 自動 記錄來源 IP' in OriScreen)
+        Log.showValue(
+            Log.Level.DEBUG,
+            '記錄來源 IP',
+            PushRecordIP
+        )
+
+        # 推文時 對齊 開頭
+        # 推文時 不用對齊 開頭
+        PushAligned = ('推文時 對齊 開頭' in OriScreen)
+        Log.showValue(
+            Log.Level.DEBUG,
+            '對齊開頭',
+            PushAligned
+        )
+
+        # 板主 可 刪除部份違規文字
+        ModeratorCanDelIllegalContent = (
+            '板主 可 刪除部份違規文字' in OriScreen
+        )
+        Log.showValue(
+            Log.Level.DEBUG,
+            '板主可刪除部份違規文字',
+            ModeratorCanDelIllegalContent
+        )
+
+        # 轉錄文章 會 自動記錄，且 需要 發文權限
+        TranPostAutoRecordedAndRequirePostPermissions = (
+            '轉錄文章 會 自動記錄，且 需要 發文權限' in OriScreen
+        )
+        Log.showValue(
+            Log.Level.DEBUG,
+            '轉錄文章 會 自動記錄，且 需要 發文權限',
+            TranPostAutoRecordedAndRequirePostPermissions
+        )
+
+        CoolMode = (
+            '未 設為冷靜模式' not in OriScreen
+        )
+        Log.showValue(
+            Log.Level.DEBUG,
+            '冷靜模式',
+            CoolMode
+        )
+
+        Require18 = (
+            '禁止 未滿十八歲進入' in OriScreen
+        )
+
+        Log.showValue(
+            Log.Level.DEBUG,
+            '禁止未滿十八歲進入',
+            Require18
+        )
+
+        p = re.compile('登入次數 [\d]+ 次以上')
+        r = p.search(OriScreen)
+        if r is not None:
+            RequireLoginTime = r.group(0).split(' ')[1]
+            RequireLoginTime = int(RequireLoginTime)
+        else:
+            RequireLoginTime = 0
+        Log.showValue(
+            Log.Level.DEBUG,
+            '發文限制登入次數',
+            RequireLoginTime
+        )
+
+        p = re.compile('退文篇數 [\d]+ 篇以下')
+        r = p.search(OriScreen)
+        if r is not None:
+            RequireIllegalPost = r.group(0).split(' ')[1]
+            RequireIllegalPost = int(RequireIllegalPost)
+        else:
+            RequireIllegalPost = 0
+        Log.showValue(
+            Log.Level.DEBUG,
+            '發文限制退文篇數',
+            RequireIllegalPost
+        )
+
+        BoardInfo = DataType.BoardInfo(
+            Board,
+            OnlineUser,
+            ChineseDes,
+            Moderators,
+            OpenState,
+            IntoTopTenWhenHide,
+            NonBoardMembersPost,
+            ReplyPost,
+            SelfDelPost,
+            PushPost,
+            BooPost,
+            FastPush,
+            MinInterval,
+            PushRecordIP,
+            PushAligned,
+            ModeratorCanDelIllegalContent,
+            TranPostAutoRecordedAndRequirePostPermissions,
+            CoolMode,
+            Require18,
+            RequireLoginTime,
+            RequireIllegalPost,
+        )
+        return BoardInfo
+
 
 if __name__ == '__main__':
 
