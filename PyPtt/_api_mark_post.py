820af4639f1a6f5011f396d62125a710ebc8b307
CodingMan <CodingMan@uptt.cc>
2020-08-06 15:15:09 +0800

    Fixed log error, mark post error in index mode

--- PyPtt/_api_mark_post.py
+++ PyPtt/_api_mark_post.py
@@ -6,6 +6,8 @@ try:
     from . import screens
     from . import command
     from . import check_value
+    from . import exceptions
+    from . import lib_util
 except ModuleNotFoundError:
     import data_type
     import i18n
@@ -14,6 +16,8 @@ except ModuleNotFoundError:
     import screens
     import command
     import check_value
+    import exceptions
+    import lib_util
 
 
 def markPost(
@@ -36,16 +40,16 @@ def markPost(
     )
 
     check_value.check(api.config, int, 'mark_type', mark_type,
-                     value_class=data_type.mark_type)
+                      value_class=data_type.mark_type)
     check_value.check(api.config, str, 'Board', board)
     if post_aid is not None:
         check_value.check(api.config, str, 'PostAID', post_aid)
     check_value.check(api.config, int, 'PostIndex', post_index)
     check_value.check(api.config, int, 'SearchType', search_type,
-                     value_class=data_type.post_search_type)
+                      value_class=data_type.post_search_type)
     if search_condition is not None:
         check_value.check(api.config, str,
-                         'SearchCondition', search_condition)
+                          'SearchCondition', search_condition)
 
     if len(board) == 0:
         raise ValueError(log.merge([
@@ -54,49 +58,61 @@ def markPost(
             board
         ]))
 
-    if post_index != 0 and isinstance(post_aid, str):
-        raise ValueError(log.merge([
-            'PostIndex',
-            'PostAID',
-            i18n.ErrorParameter,
-            i18n.BothInput
-        ]))
+    if mark_type != data_type.mark_type.DeleteD:
+        if post_index != 0 and isinstance(post_aid, str):
+            raise ValueError(log.merge(
+                api.config,
+                [
+                    'PostIndex',
+                    'PostAID',
+                    i18n.ErrorParameter,
+                    i18n.BothInput
+                ]))
 
-    if post_index == 0 and post_aid is None:
-        raise ValueError(log.merge([
-            'PostIndex',
-            'PostAID',
-            i18n.ErrorParameter
-        ]))
+        if post_index == 0 and post_aid is None:
+            raise ValueError(log.merge(
+                api.config,
+                [
+                    'PostIndex',
+                    'PostAID',
+                    i18n.ErrorParameter
+                ]))
 
     if search_condition is not None and search_type == 0:
-        raise ValueError(log.merge([
-            'SearchType',
-            i18n.ErrorParameter,
-        ]))
+        raise ValueError(log.merge(
+            api.config,
+            [
+                'SearchType',
+                i18n.ErrorParameter,
+            ]))
 
     if search_type == data_type.post_search_type.PUSH:
         try:
             S = int(search_condition)
         except ValueError:
-            raise ValueError(log.merge([
-                'SearchCondition',
-                i18n.ErrorParameter,
-            ]))
+            raise ValueError(log.merge(
+                api.config,
+                [
+                    'SearchCondition',
+                    i18n.ErrorParameter,
+                ]))
 
         if not (-100 <= S <= 110):
-            raise ValueError(log.merge([
-                'SearchCondition',
-                i18n.ErrorParameter,
-            ]))
+            raise ValueError(log.merge(
+                api.config,
+                [
+                    'SearchCondition',
+                    i18n.ErrorParameter,
+                ]))
 
     if post_aid is not None and search_condition is not None:
-        raise ValueError(log.merge([
-            'PostAID',
-            'SearchCondition',
-            i18n.ErrorParameter,
-            i18n.BothInput,
-        ]))
+        raise ValueError(log.merge(
+            api.config, [
+                'PostAID',
+                'SearchCondition',
+                i18n.ErrorParameter,
+                i18n.BothInput,
+            ]))
 
     if post_index != 0:
         NewestIndex = api._get_newest_index(
@@ -106,7 +122,7 @@ def markPost(
             search_condition=search_condition
         )
         check_value.check_index(api.config, 'PostIndex',
-                               post_index, max_value=NewestIndex)
+                                post_index, max_value=NewestIndex)
 
     if mark_type == data_type.mark_type.UNCONFIRMED:
         # 批踢踢兔沒有待證文章功能 QQ
@@ -156,6 +172,8 @@ def markPost(
     if post_aid is not None:
         cmd_list.append('#' + post_aid)
 
+        cmd_list.append(command.Enter)
+
     elif post_index != 0:
         if search_condition is not None:
             if search_type == data_type.post_search_type.KEYWORD:
@@ -174,7 +192,7 @@ def markPost(
 
         cmd_list.append(str(post_index))
 
-    cmd_list.append(command.Enter)
+        cmd_list.append(command.Enter)
 
     if mark_type == data_type.mark_type.S:
         cmd_list.append('L')
