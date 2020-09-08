34e1cdc33dca58be8b862bc768dcd1985808f88a
CodingMan <pttcodingman@gmail.com>
2020-09-08 11:54:00 +0800

    get_bottom_post_list test ok

--- PyPtt/_api_get_bottom_post_list.py
+++ PyPtt/_api_get_bottom_post_list.py
@@ -8,6 +8,7 @@ try:
     from . import screens
     from . import exceptions
     from . import command
+    from . import _api_util
 except ModuleNotFoundError:
     import data_type
     import i18n
@@ -16,19 +17,23 @@ except ModuleNotFoundError:
     import screens
     import exceptions
     import command
+    import _api_util
 
 
 def get_bottom_post_list(api, board):
-    api._goto_board(board)
+    api._goto_board(board, end=True)
 
     last_screen = api.connect_core.get_screen_queue()[-1]
 
-    print(last_screen)
+    # print(last_screen)
 
     bottom_screen = [line for line in last_screen.split('\n') if '★' in line[:8]]
     bottom_length = len(bottom_screen)
     # bottom_screen = '\n'.join(bottom_screen)
     # print(bottom_screen)
+
+    if bottom_length == 0:
+        return list()
 
     cmd_list = list()
     cmd_list.append(command.QueryPost)
@@ -61,7 +66,28 @@ def get_bottom_post_list(api, board):
         ),
     ]
 
-    index = api.connect_core.send(cmd, target_list)
-    last_screen = api.connect_core.get_screen_queue()[-1]
+    result = list()
+    for _ in range(0, bottom_length):
+        api.connect_core.send(cmd, target_list)
+        last_screen = api.connect_core.get_screen_queue()[-1]
 
-    print(last_screen)
+        lock_post, post_title, post_aid, post_web, post_money, list_date, push_number, post_index = _api_util.parse_query_post(
+            api,
+            last_screen)
+
+        current_post = api.get_post(board, post_aid=post_aid, query=True)
+
+        # print(current_post.aid)
+        # print(current_post.title)
+        # print('==========================')
+
+        result.append(current_post)
+
+        cmd_list = list()
+        cmd_list.append(command.Enter)
+        cmd_list.append(command.Up)
+        cmd_list.append(command.QueryPost)
+        cmd = ''.join(cmd_list)
+
+    return list(reversed(result))
+
