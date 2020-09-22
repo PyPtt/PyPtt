9ca5b2c0f3e25580ffe7164073b8550a15d9228d
CodingMan <pttcodingman@gmail.com>
2020-09-22 14:10:35 +0800

    Remove beautifulsoup4 requests, add single log

--- setup.py
+++ setup.py
@@ -127,8 +127,7 @@ setup(
         'progressbar2',
         'websockets',
         'uao',
-        'beautifulsoup4',
-        'requests'
+        'single_log'
     ],
 
     # List additional groups of dependencies here (e.g. development
