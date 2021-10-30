f632d0745e4513b030432d2afd8fbb8d5b75f7c8
CodingMan <pttcodingman@gmail.com>
2021-10-30 23:01:37 +0800

    Remove log.py.

--- PyPtt/_api_get_user.py
+++ PyPtt/_api_get_user.py
@@ -6,7 +6,6 @@ try:
     from . import data_type
     from . import i18n
     from . import connect_core
-    from . import log
     from . import screens
     from . import exceptions
     from . import command
@@ -14,7 +13,6 @@ except ModuleNotFoundError:
     import data_type
     import i18n
     import connect_core
-    import log
     import screens
     import exceptions
     import command
