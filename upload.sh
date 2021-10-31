c865258f9f14709843b19bf82b67026b9b7c7585
CodingMan <truth.tseng@ailabs.tw>
2021-10-31 22:30:01 +0800

    Adjust upload script.

--- upload.sh
+++ upload.sh
@@ -7,13 +7,13 @@ python3 setup.py sdist bdist_wheel --universal
 
 case $1 in
 release)
-    twine upload dist/*
+    echo upload to pypi
+    python3 -m twine upload dist/*
     ;;
 test)
     echo upload to testpypi
     python3 -m twine upload --repository testpypi dist/*
     ;;
-
 *)
     echo "unknown command [$@]"
     ;;
