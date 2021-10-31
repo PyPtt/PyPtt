c15b0e42219ac75558602afde4774cde624b06ff
CodingMan <pttcodingman@gmail.com>
2021-10-31 21:32:37 +0800

    Adjust upload script option.

--- upload.sh
+++ upload.sh
@@ -3,8 +3,20 @@
 echo PyPtt uploader v 1.0.1
 
 rm dist/*
+python3 setup.py sdist bdist_wheel --universal
 
-python3 setup.py sdist bdist_wheel
-twine upload dist/*
+case $1 in
+release)
+    twine upload dist/*
+    ;;
+test)
+    echo upload to testpypi
+    python3 -m twine upload --repository testpypi dist/*
+    ;;
+
+*)
+    echo "unknown command [$@]"
+    ;;
+esac
 
 echo Upload finish
