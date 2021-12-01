e74c3287fc93c55ace30e5f7ffacc9614ad6c74f
CodingMan <truth.tseng@ailabs.tw>
2021-12-01 14:59:55 +0800

    Add login logout test case.

---
+++ test/login_logout.py
@@ -0,0 +1,25 @@
+import time
+
+from SingleLog.log import Logger
+
+import util
+from PyPtt import PTT
+
+if __name__ == '__main__':
+    logger = Logger('TEST')
+
+    host_list = [
+        # PTT.data_type.host_type.PTT1,
+        PTT.data_type.host_type.PTT2
+    ]
+
+    for host in host_list:
+        ptt_bot = PTT.API(
+            host=host,
+            log_level=PTT.logger_level.TRACE,
+        )
+        util.login(ptt_bot, host)
+
+        time.sleep(2)
+
+        ptt_bot.logout()
