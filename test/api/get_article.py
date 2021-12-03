11efe2b83ed696f1750abf5f38ba5f1cc00e0185
CodingMan <truth.tseng@ailabs.tw>
2021-12-03 22:08:54 +0800

    Rename get article api.

---
+++ test/api/get_article.py
@@ -0,0 +1,70 @@
+from SingleLog.log import Logger
+
+import PyPtt
+import util
+from PyPtt_0_9 import PTT
+
+test_post_list = [
+    ('Python', 1),
+    # ('NotExitBoard', 1),
+    # ('Python', '1TJH_XY0'),
+    # # 文章格式錯誤
+    # ('Steam', 4444),
+    # ('Stock', 92324),
+    # ('Stock', '1TVnEivO'),
+    # # 文章格式錯誤
+    # ('movie', 457),
+    # ('Gossiping', '1UDnXefr'),
+    # ('joke', '1Tc6G9eQ'),
+    # # 135193
+    # ('Test', 575),
+    # # 待證文章
+    # ('Test', '1U3pLzi0'),
+    # # 古早文章
+    # ('LAW', 1),
+    # # 辦刪除文章
+    # ('Test', 347),
+    # # comment number parse error
+    # ('Ptt25sign', '1VppdKLW'),
+]
+
+
+def test(ptt_bot: PyPtt.API):
+    result = {}
+    for board, index in test_post_list:
+        article = ptt_bot.get_article(
+            board,
+            post_index=index)
+
+        result['title'] = article.title
+
+    return result
+
+
+def func():
+    ptt_bot_list = [
+        PyPtt.API(
+            log_level=PyPtt.LOG_LEVEL.TRACE
+        ),
+        # PTT.API()
+    ]
+
+    host_list = [
+        PyPtt.HOST.PTT1,
+        PyPtt.HOST.PTT2]
+
+    result = []
+    for ptt_bot in ptt_bot_list:
+        util.login(ptt_bot)
+
+        result.append(test(ptt_bot))
+
+        ptt_bot.logout()
+
+    logger.info(result)
+    # assert (result[0] == result[1])
+
+
+if __name__ == '__main__':
+    logger = Logger('TEST')
+    func()
