af796ce871c5d0f2b3892e417e4dcdb956a66a20
PttCodingMan <pttcodingman@gmail.com>
2022-08-06 15:12:46 +0800

    Add get user test case.

---
+++ tests/api/get_user.py
@@ -0,0 +1,37 @@
+import PyPtt
+from tests import util
+
+
+def test(ptt_bot: PyPtt.API):
+    test_users = [
+        'CodingMan',
+    ]
+
+    for test_user in test_users:
+        user_info = ptt_bot.get_user(test_user)
+        print(user_info)
+
+
+def func():
+    host_list = [
+        PyPtt.HOST.PTT1,
+        # PyPtt.HOST.PTT2
+    ]
+
+    for host in host_list:
+        ptt_bot = PyPtt.API(
+            host=host,
+            # log_level=PyPtt.LOG_LEVEL.TRACE,
+        )
+        try:
+            util.login(ptt_bot)
+
+            test(ptt_bot)
+
+            ptt_bot.logout()
+        finally:
+            ptt_bot.logout()
+
+
+if __name__ == '__main__':
+    func()
