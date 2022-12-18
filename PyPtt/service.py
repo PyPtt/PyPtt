c3561526f6d697ea83716d9e27ba9f42eeedecea
CodingMan <pttcodingman@gmail.com>
2022-12-18 17:11:40 +0800

    Add PyPtt service.

--- PyPtt/service.py
+++ PyPtt/service.py
@@ -1,3 +1,4 @@
+import threading
 import time
 import uuid
 
@@ -6,34 +7,68 @@ from SingleLog import Logger
 import PyPtt
 
 
-class PyPttService:
+class Service:
 
     def __init__(self, ):
         self._log = Logger('Service')
-        self._api = PyPtt.API()
+        self._log.info('init')
+
+        self._api = None
         self._call_queue = []
         self._call_result = {}
+
+        self._id_pool = set()
+        self._id_pool_lock = threading.Lock()
+
         self._close = False
+
+        self._thread = threading.Thread(target=self._run, daemon=True)
+        self._thread.start()
 
     def _run(self):
 
+        if self._api is not None:
+            self._api.logout()
+            self._api = None
+
+        self._api = PyPtt.API()
+
+        self._log.info('start')
+
         while not self._close:
             if len(self._call_queue) == 0:
-                time.sleep(0.1)
+                time.sleep(0.05)
                 continue
 
             call = self._call_queue.pop(0)
 
             func = getattr(self._api, call['api'])
 
-            result = func(**call['args'])
+            api_result = None
+            api_exception = None
+            try:
+                api_result = func(**call['args'])
+            except Exception as e:
+                api_exception = e
 
-            self._call_result[call['id']] = result
+            self._call_result[call['id']] = {
+                'result': api_result,
+                'exception': api_exception
+            }
 
-    def call(self, api: str, args: dict = None):
+    def _get_call_id(self):
+        while True:
+            call_id = uuid.uuid4().hex
+
+            with self._id_pool_lock:
+                if call_id not in self._id_pool:
+                    self._id_pool.add(call_id)
+                    return call_id
+
+    def call(self, api: str, args: dict = {}):
         call = {
             'api': api,
-            'id': uuid.uuid4().hex,
+            'id': self._get_call_id(),
             'args': args
         }
         self._call_queue.append(call)
@@ -41,14 +76,17 @@ class PyPttService:
         while call['id'] not in self._call_result:
             time.sleep(0.1)
 
-        result = self._call_result[call['id']]
+        call_result = self._call_result[call['id']]
         del self._call_result[call['id']]
 
-        return result
+        with self._id_pool_lock:
+            self._id_pool.remove(call['id'])
+
+        if call_result['exception'] is not None:
+            raise call_result['exception']
+
+        return call_result['result']
 
     def close(self):
+        self._log.info('close')
         self._close = True
-
-
-if __name__ == '__main__':
-    print(uuid.uuid4().hex)
