f233bd8741c35a1e876733424af03bdba8f2b660
CodingMan <pttcodingman@gmail.com>
2022-12-18 17:12:51 +0800

    Add test case of service.

---
+++ tests/service/test_service.py
@@ -0,0 +1,46 @@
+import threading
+
+from SingleLog import Logger
+
+import PyPtt
+from PyPtt import Service
+from tests import config
+
+
+def api_test(thread_id, service):
+    logger = Logger(f'api_test_{thread_id}')
+
+    result = service.call('get_time')
+    logger.info('get_time', result)
+
+    result = service.call('get_aid_from_url', {'url': 'https://www.ptt.cc/bbs/Python/M.1565335521.A.880.html'})
+    logger.info('get_aid_from_url', result)
+
+    result = service.call('get_newest_index', {'index_type': PyPtt.NewIndex.BOARD, 'board': 'Python'})
+    logger.info('get_newest_index', result)
+
+
+def test():
+    service = Service()
+
+    try:
+        service = Service()
+
+        service.call('login', {'ptt_id': config.PTT1_ID, 'ptt_pw': config.PTT1_PW})
+
+        pool = []
+        for i in range(10):
+            t = threading.Thread(target=api_test, args=(i, service))
+            t.start()
+            pool.append(t)
+
+        for t in pool:
+            t.join()
+
+        service.call('logout')
+    finally:
+        service.close()
+
+
+if __name__ == '__main__':
+    test()
