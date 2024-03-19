9a5b66866120cfa691ee8111d836b4b856ce275a
CodingMan <pttcodingman@gmail.com>
2024-03-19 16:36:35 +0800

    Feat/adjust deploy flow conditions (#127)
    * feat: adjust deploy flow condition.
    
    * chore: adjust condition.
    
    * chore: test new flow condition.
    
    * chore: adjust dev version logic.
    
    * adjust
    
    * chore: add commit_hash temp.
    
    * chore: adjust commit file path.
    
    * chore: adjust.
    
    * chore: commit.

--- scripts/package_script.py
+++ scripts/package_script.py
@@ -2,10 +2,8 @@ import os
 import subprocess
 import time
 
-from setuptools import setup
 
-
-def version_automation_script():
+def get_next_version():
     is_merged = os.environ.get('GITHUB_EVENT_NAME') == 'pull_request' and os.environ.get(
         'GITHUB_EVENT_ACTION') == 'closed'
     print('is_merged:', is_merged)
@@ -72,55 +70,3 @@ def version_automation_script():
         f.write('\n')
 
     return version
-
-
-version = version_automation_script()
-print('the next version:', version)
-
-setup(
-    name='PyPtt',  # Required
-    version=version,  # Required
-    description='PyPtt\ngithub: https://github.com/PyPtt/PyPtt',  # Required
-    long_description=open('README.md', encoding="utf-8").read(),  # Optional
-    long_description_content_type='text/markdown',
-    url='https://pyptt.cc/',  # Optional
-
-    author='CodingMan',  # Optional
-    author_email='pttcodingman@gmail.com',  # Optional
-    # https://pypi.org/classifiers/
-    classifiers=[  # Optional
-
-        'Development Status :: 5 - Production/Stable',
-
-        'Operating System :: OS Independent',
-
-        'Intended Audience :: Developers',
-        'Topic :: Communications :: BBS',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Internet',
-
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3 :: Only',
-
-        'Natural Language :: Chinese (Traditional)',
-        'Natural Language :: English',
-    ],
-    keywords=['PTT', 'crawler', 'bot', 'library', 'websockets'],  # Optional
-
-    python_requires='>=3.8',
-    packages=['PyPtt'],
-    install_requires=[
-        'progressbar2',
-        'websockets',
-        'uao',
-        'SingleLog',
-        'requests',
-        'AutoStrEnum'
-    ],
-)
