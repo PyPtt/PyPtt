22a7fedd55efff74a72816c4b054cd8532eb3cb9
CodingMan <pttcodingman@gmail.com>
2024-04-11 15:22:02 +0800

    feat: adjust ssl key algo from RSA to EC. (#134)

--- scripts/gen_ssl.sh
+++ scripts/gen_ssl.sh
@@ -1,5 +1,8 @@
 
-openssl genrsa -out key.pem 2048
+#openssl genrsa -out key.pem 2048
+openssl ecparam -genkey -name prime256v1 -out key.pem
+#openssl genpkey -algorithm ed25519 -out key.pem
+
 openssl req -new -key key.pem -out csr.pem
 openssl x509 -req -days 3650 -in csr.pem -signkey key.pem -out cert.pem
 
